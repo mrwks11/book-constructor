e8e56ebcd626623ecac8d2cac469b46d68306a35
mrwks11 <w.k.sinke@gmail.com>
2020-10-08 09:46:10 +0200

    Added read status button & styled elements

--- script.js
+++ script.js
@@ -1,115 +1,125 @@
 // BOOK CLASS
 class Book {
-    constructor(title, author, pages, isbn, read) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.isbn = isbn
-        this.read = read
-    }
+  constructor(title, author, pages, isbn, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isbn = isbn;
+    this.read = read;
+  }
 }
 
 // UI CLASS
 class UI {
-    static displayBooks() {
-        const books = Store.getBooks();
+  static displayBooks() {
+    const books = Store.getBooks();
 
-        books.forEach((book) => UI.addBookToLibrary(book));
-    }
+    books.forEach((book) => UI.addBookToLibrary(book));
+  }
 
-    // Create elements and add book to UI (my library)
-    static addBookToLibrary(book) {
-        const library = document.querySelector('#library'); 
+  // Create elements and add book to UI (my library)
+  static addBookToLibrary(book) {
+    const library = document.querySelector('#library');
 
-        const row = document.createElement('tr');
-        row.dataset.isbn = book.isbn;
+    const row = document.createElement('tr');
+    row.dataset.isbn = book.isbn;
 
-        row.innerHTML = `
+    row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.pages}</td>
             <td>${book.isbn}</td>
-            <td>${book.read}</td>
-            <td><button class="remove-btn" data-isbn="${book.isbn}">Remove</button></td>
+            <td><button class="btn read-btn">${book.read}</button></td>
+            <td><button class="btn remove-btn" data-isbn="${book.isbn}">Remove</button></td>
         `;
 
-        library.appendChild(row);
-    }
+    library.appendChild(row);
+  }
 
-    // Show error or success message
-    static alertMessage(message, className) {
-        const container = document.querySelector('.container')
-        const form = document.querySelector('#book-form');
-        const div = document.createElement('div');
-        div.textContent = message;
-        div.classList.add(className);
+  // Show error or success message
+  static alertMessage(message, className) {
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    const div = document.createElement('div');
+    div.textContent = message;
+    div.classList.add(className);
 
-        if (className === 'error') {
-            container.insertBefore(div, form);
-            setTimeout(() => {
-                div.remove();
-            }, 3000);
-        } else {
-            container.insertBefore(div, form);
-            setTimeout(() => {
-                div.remove();
-            }, 3000);
-        }
-        
+    if (className === 'error') {
+      container.insertBefore(div, form);
+      setTimeout(() => {
+        div.remove();
+      }, 2000);
+    } else {
+      container.insertBefore(div, form);
+      setTimeout(() => {
+        div.remove();
+      }, 2000);
     }
+  }
 
-    static checkRadioButtonValue(read) {
-        if (read.checked === true) {
-            return 'yes';
-        } else {
-            return 'no';
-        }
-    }
+  // Check read status
+  static checkReadStatus(read) {
+    return read.checked ? 'yes' : 'no';
+  }
 
-    // Remove book from UI
-    static removeBook(target) {
-        if (target.classList.contains('remove-btn')) {
-        target.parentElement.parentElement.remove();
-        } else {
-            return;
-        }
+  // Change read status in UI
+  static changeReadStatus(target) {
+    if (target.classList.contains('read-btn')) {
+      target.textContent === 'yes'
+        ? (target.textContent = 'no')
+        : (target.textContent = 'yes');
+      // Success message
+      UI.alertMessage('Read status changed', 'success');
+    } else {
+      return;
     }
+  }
+
+  // Remove book from UI
+  static removeBook(target) {
+    if (target.classList.contains('remove-btn')) {
+      target.parentElement.parentElement.remove();
+      // Success message
+      UI.alertMessage('Book removed', 'success');
+    } else {
+      return;
+    }
+  }
 }
 
-// STORE LIBRARY CLASS (
-// local storage can't store arrays, only strings
+// STORE LIBRARY CLASS (local storage can't store arrays, only strings)
 class Store {
-    static getBooks() {
-        let books;
-        if (localStorage.getItem('books') === null) {
-            books = [];
-        } else {
-            // Convert string to array
-            books = JSON.parse(localStorage.getItem('books'));
-        }
-
-        return books;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      // Convert string to array
+      books = JSON.parse(localStorage.getItem('books'));
     }
 
-    static addBook(book) {
-        const books = Store.getBooks();
-        books.push(book);
-        // Convert array to string
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+    return books;
+  }
 
-    static removeBook(isbn) {
-        // Remove by ISBN because it's unique
-        const books = Store.getBooks();
-        // Loop through books and remove book with ISBN-match
-        books.forEach((book, index) => {
-            if (book.isbn === isbn) {
-                books.splice(index, 1);
-            }
-        });
-        // Convert array to string
-        localStorage.setItem('books', JSON.stringify(books));
-    }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    // Convert array to string
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    // Remove by ISBN because it's unique
+    const books = Store.getBooks();
+    // Loop through books and remove book with ISBN-match
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    // Convert array to string
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 // Event: display books
@@ -118,57 +128,50 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks());
 // Event: add a book
 const form = document.querySelector('#book-form');
 form.addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
+  // Prevent actual submit
+  e.preventDefault();
 
-    // Get form values    
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const pages = document.querySelector('#pages').value;
-    const isbn = document.querySelector('#isbn').value;
-    let read = document.querySelector('#read');
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const isbn = document.querySelector('#isbn').value;
+  let read = document.querySelector('#read');
 
-    // Check radio button value
-    read = UI.checkRadioButtonValue(read)
+  // Check radio button value
+  read = UI.checkReadStatus(read);
 
-    // Check if field is empty
-    if (title === '' || 
-        author === '' || 
-        pages === '' ||
-        isbn === '') {
-        
-        // Initiate error message
-        UI.alertMessage('Please fill in all fields', 'error');
+  // Check if field is empty
+  if (title === '' || author === '' || pages === '' || isbn === '') {
+    // Initiate error message
+    UI.alertMessage('Please fill in all fields', 'error');
+  } else {
+    // Initiate success message
+    UI.alertMessage('Book added', 'success');
 
-    } else {
+    // Create new book with form input
+    let book = new Book(title, author, pages, isbn, read);
 
-        // Initiate success message
-        UI.alertMessage('Book added', 'success');
+    // Add book to UI (my library)
+    UI.addBookToLibrary(book);
 
-        // Create new book with form input
-        let book = new Book(title, author, pages, isbn, read);
+    // Add book to local storage
+    Store.addBook(book);
 
-        // Add book to UI (my library)
-        UI.addBookToLibrary(book);
-
-        // Add book to local storage
-        Store.addBook(book);
-
-        // Clear UI form fields
-        form.reset();
-        }
-
+    // Clear UI form fields
+    form.reset();
+  }
 });
 
-// Event: remove a book
+// Event: change read status & remove a book
 const library = document.querySelector('#library');
 library.addEventListener('click', (e) => {
-    // Remove from UI
-    UI.removeBook(e.target);
+  // Change read status UI
+  UI.changeReadStatus(e.target);
 
-    // Remove from local storage
-    Store.removeBook( e.target.dataset.isbn )
+  // Remove from UI
+  UI.removeBook(e.target);
 
-    // Success message
-    UI.alertMessage('Book removed', 'success');
+  // Remove from local storage
+  Store.removeBook(e.target.dataset.isbn);
 });
